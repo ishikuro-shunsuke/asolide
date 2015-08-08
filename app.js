9274c786246ae3dec20a4bc0be9758caa8a32917
Shunsuke Ishikuro <m5181135@u-aizu.ac.jp>
2015-08-08 15:54:42 +0900

    add log feature

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 var bodyParser = require('body-parser');
+var io = require('socket.io')(http);
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 var compile = {};
@@ -11,34 +13,37 @@ app.use(express.static(__dirname + '/views'));
 
 app.post('/exec', function (req, res) {
   if (compile.kill) {
-    console.log('Recompiling...')
     compile.kill('SIGINT');
-  } else {
-    console.log('Compiling...')
   }
+  console.log('Compiling');
+  io.emit('system', 'Compiling');
 
   var header = '#include "../resources/DE2_115.nsh"\n';
   var source = header + req.body.source;
   fs.writeFile('workspace/DE2_115.nsl', source, function (err) {
     if (err) {
       console.log(err);
+      io.emit('stderr', err);
     }
 
     compile = spawn('bash', ['./resources/ide.sh']);
     compile.stdout.on('data', function (data) {
       console.log(data.toString());
+      io.emit('stdout', data.toString());
     });
     compile.stderr.on('data', function (data) {
       console.log(data.toString());
+      io.emit('stderr', data.toString());
     });
     compile.on('close', function() {
-      console.log('Finished.');
+      console.log('Finished');
+      io.emit('system', 'Finished');
       res.send('finished');
     });
   });
 });
 
-var server = app.listen(3000, function () {
+var server = http.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
